108a039aae8c357254fc3b328308d6623fd2250e
Gordon Lay <gal65@uclive.ac.nzcd>
2019-10-09 16:29:26 +1300

    Added file

--- game.c
+++ game.c
@@ -2,12 +2,10 @@
 #include "system.h"
 #include "tinygl.h"
 #include "task.h"
-//#include "navswitch.h"
-//#include "flasher.h"
-//#include "eeprom.h"
+#include "navswitch.h"
+#include "eeprom.h"
 #include "uint8toa.h"
 #include "../fonts/font3x5_1.h"
-//#include "tweeter.h"
 //#include "mmelody.h"
 #include "pio.h"
 #include "pacer.h"
@@ -19,16 +17,17 @@
 int main (void)
 {
     system_init();
+    pacer_init (PACER_RATE);
 
     tinygl_init(PACER_RATE);
     tinygl_font_set(&font3x5_1);
+    tinygl_text_dir_set (TINYGL_TEXT_DIR_ROTATE);
     tinygl_text_speed_set(MESSAGE_RATE);
     tinygl_text_mode_set(TINYGL_TEXT_MODE_SCROLL);
 
-    tinygl_text("Super Dodgeball 9000");
+    tinygl_text("SUPER DODGEBALL 9000");
 
 
-    pacer_init (PACER_RATE);
 
     while(1)
     {
