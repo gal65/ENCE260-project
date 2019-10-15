5a6010801bf8d517eb23d425cbf84fedb8284411
Gordon Lay <gal65@uclive.ac.nzcd>
2019-10-15 23:29:47 +1300

    Working intro, ready, and game start states

--- game.c
+++ game.c
@@ -10,105 +10,99 @@
 #include <avr/io.h>
 #include "tweeter.h"
 #include "mmelody.h"
+#include "button.h"
 #include "pio.h"
+#include <stdbool.h>
+#include "ir_serial.h"
+#include "ir.h"
 
 #define PACER_RATE 500
-#define MESSAGE_RATE 15
-enum {TUNE_BPM_RATE = 200};
-enum {TUNE_TASK_RATE = 100};
-enum {TWEETER_TASK_RATE = 5000};
+#define MESSAGE_RATE 20
 
-/* Connect piezo tweeter to eight and sixth pin of UCFK4 P1 connector
-   for push-pull operation.  For single-ended drive (with reduced
-   volume) connect the other piezo connection to ground or Vcc and do
-   not define PIEZO2_PIO.  */
-#define PIEZO1_PIO PIO_DEFINE (PORT_D, 1)
-#define PIEZO2_PIO PIO_DEFINE (PORT_D, 3)
+static _Bool ready;
+static _Bool handshake;
 
-static tweeter_scale_t scale_table[] = TWEETER_SCALE_TABLE (TWEETER_TASK_RATE);
-static tweeter_t tweeter;
-static mmelody_t melody;
-static mmelody_obj_t melody_info;
-static tweeter_obj_t tweeter_info;
-
-static const char game_intro_tune[] =
+static void handshake_init (void)
 {
-#include "intro_music.mmel"
-"> "
-};
-
-static void tweeter_task_init (void)
-{
-    tweeter = tweeter_init (&tweeter_info, TWEETER_TASK_RATE, scale_table);
-
-    pio_config_set (PIEZO1_PIO, PIO_OUTPUT_LOW);
-#ifdef PIEZO2_PIO
-    pio_config_set (PIEZO2_PIO, PIO_OUTPUT_LOW);
-#endif
+    handshake = false;
 }
 
-
-static void tweeter_task (__unused__ void *data)
+static void ready_screen_init(void)
 {
-    bool state;
-
-    state = tweeter_update (tweeter);
-
-    pio_output_set (PIEZO1_PIO, state);
-#ifdef PIEZO2_PIO
-
-    pio_output_set (PIEZO2_PIO, !state);
-#endif
+    ready = false;
 }
 
-
-static void tune_task_init (void)
+static void ready_screen_task(void)
 {
-    melody = mmelody_init (&melody_info, TUNE_TASK_RATE,
-               (mmelody_callback_t) tweeter_note_play, tweeter);
-
-    mmelody_speed_set (melody, TUNE_BPM_RATE);
-}
-
-static void tune_task (__unused__ void *data)
-{
-    mmelody_update (melody);
+    ready = true;
 }
 
 static void tinygl_task_init(void)
 {
-    tinygl_init(PACER_RATE);
     tinygl_font_set(&font3x5_1);
-    tinygl_text_dir_set (TINYGL_TEXT_DIR_ROTATE);
     tinygl_text_speed_set(MESSAGE_RATE);
     tinygl_text_mode_set(TINYGL_TEXT_MODE_SCROLL);
+    tinygl_text_dir_set (TINYGL_TEXT_DIR_ROTATE);
+}
+
+static void show_err (uint8_t err)
+{
+    char buffer[3];
+
+    buffer[0] = 'E';
+    buffer[1] = err + '0';
+    buffer[2] = 0;
+    tinygl_text (buffer);
+}
+
+static void ready_loop (void)
+{
+    tinygl_clear();
+    tinygl_text("READY?");
+    ir_serial_transmit (1);
+    while (1) {
+        uint8_t data;
+        pacer_wait();
+        tinygl_update();
+        ir_serial_ret_t ret;
+        ret = ir_serial_receive (&data);
+        if (ret == IR_SERIAL_OK) {
+            if (data == 1) {
+                ir_serial_transmit (1);
+                return;
+            }
+        } else if (ret < 0) {
+            show_err (-ret);
+        }
+    }
 }
 
 int main (void)
 {
-    task_t tasks[] =
-    {
-        {.func = tweeter_task, .period = TASK_RATE / TWEETER_TASK_RATE,
-         .data = 0},
-        {.func = tune_task, .period = TASK_RATE / TUNE_TASK_RATE,
-         .data = 0},
-    };
-
+    ready_screen_init();
+    button_init();
     system_init();
+    handshake_init();
+    ir_serial_init();
     pacer_init (PACER_RATE);
+    tinygl_init(PACER_RATE);
     tinygl_task_init();
-    tweeter_task_init();
-    tune_task_init();
+    //Intro screen
     tinygl_text("SUPER DODGEBALL 9000");
 
     while(1)
     {
         pacer_wait();
         tinygl_update();
-        mmelody_update (melody);
+        button_update();
+        //Break out of Intro screen and into the Ready loop
+        if (button_push_event_p(BUTTON1) && !ready && !handshake) {
+            ready_screen_task();
+            ready_loop();
+            tinygl_clear();
+            tinygl_text("GAME START!");
+        }
     }
-    task_schedule (tasks, ARRAY_SIZE (tasks));
-    mmelody_play (melody, game_intro_tune);
     return 0;
 
 }
