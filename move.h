b2cccaa8b0d05ee165562b6236ba1f9c82b5c077
Gordon Lay <gal65@uclive.ac.nzcd>
2019-10-16 01:59:11 +1300

    Move moduel header

---
+++ move.h
@@ -0,0 +1,15 @@
+#ifndef MOVE_H
+#define MOVE_H
+
+#include "system.h"
+
+//Move right
+void moveRight(uint8_t count);
+
+//Move left
+void moveLeft(uint8_t count);
+
+//Move in general
+int move(void);
+
+#endif
