9102d86e247263d20e4eee21c27cd990e1f18ccb
Gordon Lay <gal65@uclive.ac.nzcd>
2019-10-16 01:57:17 +1300

    Works all the way up until the dood be moving

--- game.c
+++ game.c
@@ -15,28 +15,27 @@
 #include <stdbool.h>
 #include "ir_serial.h"
 #include "ir.h"
+#include "move.h"
 
 #define PACER_RATE 500
 #define MESSAGE_RATE 20
 
+//Ready pending bool
 static _Bool ready;
-static _Bool handshake;
 
-static void handshake_init (void)
-{
-    handshake = false;
-}
-
+//Initialise the ready bool to false
 static void ready_screen_init(void)
 {
     ready = false;
 }
 
+//Task: ready is now true
 static void ready_screen_task(void)
 {
     ready = true;
 }
 
+//Initialising tinygl to scroll and rotate
 static void tinygl_task_init(void)
 {
     tinygl_font_set(&font3x5_1);
@@ -45,6 +44,7 @@ static void tinygl_task_init(void)
     tinygl_text_dir_set (TINYGL_TEXT_DIR_ROTATE);
 }
 
+//IR error message
 static void show_err (uint8_t err)
 {
     char buffer[3];
@@ -55,6 +55,8 @@ static void show_err (uint8_t err)
     tinygl_text (buffer);
 }
 
+//Ready-state loop; shows message READY? while sending and checking for
+//1's transmitted via serial ir
 static void ready_loop (void)
 {
     tinygl_clear();
@@ -77,18 +79,25 @@ static void ready_loop (void)
     }
 }
 
+//The game function
+static void game (void)
+{
+    move();
+}
+
+
 int main (void)
 {
+    //Initiation
     ready_screen_init();
     button_init();
     system_init();
-    handshake_init();
     ir_serial_init();
     pacer_init (PACER_RATE);
     tinygl_init(PACER_RATE);
     tinygl_task_init();
     //Intro screen
-    tinygl_text("SUPER DODGEBALL 9000");
+    tinygl_text("CATCHIN BALL Z");
 
     while(1)
     {
@@ -96,14 +105,15 @@ int main (void)
         tinygl_update();
         button_update();
         //Break out of Intro screen and into the Ready loop
-        if (button_push_event_p(BUTTON1) && !ready && !handshake) {
+        if (button_push_event_p(BUTTON1) && !ready) {
             ready_screen_task();
             ready_loop();
+            //When both players are ready, start the game
             tinygl_clear();
             tinygl_text("GAME START!");
+            game();
         }
     }
     return 0;
-
 }
 
